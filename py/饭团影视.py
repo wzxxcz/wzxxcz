7a84b7387185994f1f381b49928ae04e2a80599c
wzxxcz <155713461+wzxxcz@users.noreply.github.com>
2025-08-25 17:29:40 +0800

    Update 饭团影视.py

--- py/饭团影视.py
+++ py/饭团影视.py
@@ -3,8 +3,8 @@
 
 """
 
-作者 丢丢喵 🚓 内容均从互联网收集而来 仅供交流学习使用 版权归原创者所有 如侵犯了您的权益 请通知作者 将及时删除侵权内容
-                    ====================Diudiumiao====================
+作者 小橙子 🍊 内容均从互联网收集而来 仅供交流学习使用 版权归原创者所有 如侵犯了您的权益 请通知作者 将及时删除侵权内容
+                    ====================xcz====================
 
 """
 
@@ -216,7 +216,7 @@ class Spider(Spider):
                         "vod_id": id,
                         "vod_name": name,
                         "vod_pic": pic,
-                        "vod_remarks": '▶️' + remark
+                        "vod_remarks": '🍊' + remark
                              }
                     videos.append(video)
 
@@ -275,7 +275,7 @@ class Spider(Spider):
                         "vod_id": id,
                         "vod_name": name,
                         "vod_pic": pic,
-                        "vod_remarks": '▶️' + remark
+                        "vod_remarks": '🍊' + remark
                             }
                     videos.append(video)
 
@@ -308,7 +308,7 @@ class Spider(Spider):
         name = self.extract_middle_text(code, "s1='", "'", 0)
         Jumps = self.extract_middle_text(code, "s2='", "'", 0)
 
-        content = '😸🎉剧情介绍📢' + self.extract_middle_text(res,'剧情介绍:','">', 0)
+        content = '🍊小橙子为您介绍剧情👉请不要相信视频中的广告，以免上当受骗！' + self.extract_middle_text(res,'剧情介绍:','">', 0)
 
         director = self.extract_middle_text(res, '导演：', '</li>',1,'target=".*?">(.*?)</a>')
 
@@ -471,6 +471,7 @@ class Spider(Spider):
         elif params['type'] == "ts":
             return self.proxyTs(params)
         return None
+
 
 
 
