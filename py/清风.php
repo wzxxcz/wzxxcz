cba3a5f0390e2a5d67f4da1a8606b0e8d9dbb687
wzxxcz <155713461+wzxxcz@users.noreply.github.com>
2026-03-16 12:05:04 +0800

    Create 清风.php

---
+++ py/清风.php
@@ -0,0 +1,32 @@
+{
+    "class": [
+        {
+            "type_id": "1",
+            "type_name": "电影"
+        },
+        {
+            "type_id": "2",
+            "type_name": "电视剧"
+        },
+        {
+            "type_id": "3",
+            "type_name": "综艺"
+        },
+        {
+            "type_id": "4",
+            "type_name": "动漫"
+        },
+        {
+            "type_id": "37",
+            "type_name": "短剧"
+        },
+        {
+            "type_id": "38",
+            "type_name": "纪录片"
+        },
+        {
+            "type_id": "39",
+            "type_name": "影视解说"
+        }
+    ]
+}
