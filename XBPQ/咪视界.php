e003c5ff170fea2a8ac5e77b30c544ff71ee874d
wzxxcz <155713461+wzxxcz@users.noreply.github.com>
2026-04-14 09:09:07 +0800

    Create 咪视界.php

---
+++ XBPQ/咪视界.php
@@ -0,0 +1,28 @@
+{
+    "class": [
+        {
+            "type_id": "1000",
+            "type_name": "电影"
+        },
+        {
+            "type_id": "1001",
+            "type_name": "电视剧"
+        },
+        {
+            "type_id": "1007",
+            "type_name": "动漫"
+        },
+        {
+            "type_id": "601382",
+            "type_name": "少儿"
+        },
+        {
+            "type_id": "1005",
+            "type_name": "综艺"
+        },
+        {
+            "type_id": "1002",
+            "type_name": "纪录片"
+        }
+    ]
+}
