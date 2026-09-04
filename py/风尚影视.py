40202bdb2f52dae619cb3c35dcf582c1ffcca189
wzxxcz <155713461+wzxxcz@users.noreply.github.com>
2026-09-04 15:14:56 +0800

    Add files via upload

---
+++ py/风尚影视.py
@@ -0,0 +1,10 @@
+<style>
+            p {
+              margin: 10px 0;
+              color: white;
+              background-color: black;
+            }
+          </style>
+          <p>DevSidecar Error:</p>
+          <p>目标网站请求错误：【UNKNOWN】 连接超时: GET ➜ https://raw.githubusercontent.com:443/phu889/kjh998/refs/heads/main/py/%E9%A3%8E%E5%B0%9A%E5%BD%B1%E8%A7%86.py, cost: 7002 ms</p>
+          <p>目标地址：https://raw.githubusercontent.com:443/phu889/kjh998/refs/heads/main/py/%E9%A3%8E%E5%B0%9A%E5%BD%B1%E8%A7%86.py</p>
