a630780977fc4cca94aa0962bfb2923bcd733a74
wzxxcz <155713461+wzxxcz@users.noreply.github.com>
2026-09-10 17:20:29 +0800

    Add files via upload

---
+++ py/毒舌短剧🍀TG @hshsjk9.py
@@ -0,0 +1,10 @@
+<style>
+            p {
+              margin: 10px 0;
+              color: white;
+              background-color: black;
+            }
+          </style>
+          <p>DevSidecar Error:</p>
+          <p>目标网站请求错误：【UNKNOWN】 连接超时: GET ➜ https://raw.githubusercontent.com:443/FGBLH/EHR663/refs/heads/main/py/%E6%AF%92%E8%88%8C%E7%9F%AD%E5%89%A7%F0%9F%8D%80TG%20%40hshsjk9.py, cost: 7005 ms</p>
+          <p>目标地址：https://raw.githubusercontent.com:443/FGBLH/EHR663/refs/heads/main/py/%E6%AF%92%E8%88%8C%E7%9F%AD%E5%89%A7%F0%9F%8D%80TG%20%40hshsjk9.py</p>
