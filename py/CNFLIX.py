14be70032ee8ff8a51e10c113c09187152426873
wzxxcz <155713461+wzxxcz@users.noreply.github.com>
2026-09-26 17:41:55 +0800

    Add files via upload

---
+++ py/CNFLIX.py
@@ -0,0 +1,10 @@
+<style>
+            p {
+              margin: 10px 0;
+              color: white;
+              background-color: black;
+            }
+          </style>
+          <p>DevSidecar Error:</p>
+          <p>目标网站请求错误：【ECONNRESET】 read ECONNRESET</p>
+          <p>目标地址：https://raw.githubusercontent.com:443/phu889/kjh998/refs/heads/main/py/CNFLIX.py</p>
